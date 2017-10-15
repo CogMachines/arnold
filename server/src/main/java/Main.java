bb8064ca0a5d9e298396d530acc34aef83b0f7cf
Gene <diskzero@BubbleFish.local>
2017-10-15 16:15:49 -0700

    Server work

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,15 @@
-import static spark.Spark.*;
+
+import ai.grakn.GraknGraph;
+
+import static spark.Spark.port;
+import static spark.Spark.post;
+import static spark.Spark.get;
 
 // sudo kill $(sudo lsof -t -i:8080)
 
 public class Main {
+
+    private static GraknGraph mGraph;
 
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
